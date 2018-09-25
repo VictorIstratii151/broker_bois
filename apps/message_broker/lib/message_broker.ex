fa2aaa3d575ba69aa34697164293a673c695471f
Victor Istratii <victor.istratii@faf.utm.md>
2018-09-25 14:32:54 +0300

    Defined the functions for establishing a single tcp connection and pringing the received messages.

--- apps/message_broker/lib/message_broker.ex
+++ apps/message_broker/lib/message_broker.ex
@@ -1,18 +1,23 @@
 defmodule MessageBroker do
-  @moduledoc """
-  Documentation for MessageBroker.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
-  ## Examples
+    Logger.info("Accepting packets on port #{port}")
+    loop_acceptor(socket)
+  end
 
-      iex> MessageBroker.hello()
-      :world
+  defp loop_acceptor(socket) do
+    {:ok, client_socket} = :gen_tcp.accept(socket)
+    serve(client_socket)
+    loop_acceptor(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp serve(socket) do
+    {:ok, packet} = :gen_tcp.recv(socket, 0)
+    IO.inspect(packet)
+    serve(socket)
   end
 end
