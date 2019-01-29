e83b5a4a947d69dcaacefa4f2284d85d9b561f6e
Victor Istratii <victor.istratii@faf.utm.md>
2019-01-30 00:14:55 +0200

    The receiver can now sobscribe and publish.

--- apps/message_receiver/lib/message_receiver.ex
+++ apps/message_receiver/lib/message_receiver.ex
@@ -1,12 +1,45 @@
 defmodule MessageReceiver do
-  @host 'project.microlab.club'
+  @local 'localhost'
+
+  # use GenServer
+  require Logger
+
   @local 'localhost'
 
   def start(port) do
-    opts = [:binary, active: true, packet: 0]
+    opts = [:binary, packet: 0, active: false, reuseaddr: true]
     {:ok, socket} = :gen_tcp.connect(@local, port, opts)
+    Task.start_link(fn -> loop(socket) end)
     socket
-    # :ok = :gen_tcp.send(socket, "HELLO THERE")
-    # :ok = :gen_tcp.close(socket)
+  end
+
+  def send_msg(socket, data) do
+    :gen_tcp.send(socket, data)
+  end
+
+  def subscribe(socket, topic) do
+    data = "subscribe" <> "," <> topic
+    send_msg(socket, data)
+  end
+
+  def publish(socket, topic, payload) do
+    data = "publish" <> "," <> topic <> "," <> payload
+    send_msg(socket, data)
+  end
+
+  def loop(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, packet} ->
+        IO.inspect("Inpsecting the packet:")
+        IO.inspect(packet)
+
+      {:error, :closed} ->
+        Logger.info("Connection closed.")
+
+      something ->
+        IO.inspect(something)
+    end
+
+    loop(socket)
   end
 end
