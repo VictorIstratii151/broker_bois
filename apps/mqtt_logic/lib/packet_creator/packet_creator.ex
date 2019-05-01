ff7b6dfa1c06aeefcff6c05e92c59e5f0a23721c
Victor Istratii <victor.istratii@faf.utm.md>
2019-05-01 19:47:12 +0300

    Redefined the clean session connect function.

--- apps/mqtt_logic/lib/packet_creator/packet_creator.ex
+++ apps/mqtt_logic/lib/packet_creator/packet_creator.ex
@@ -1,27 +1,13 @@
 defmodule PacketCreator do
   use Bitwise
+  import MqttLogic
 
-  @offset 7
-  @protocol_name_length_msb 0
-  @protocol_name_length_lsb 4
-  @protocol_name "MQTT"
-  @protocol_level 4
-  @msg_keepalive_msb 0
-  @msg_keepalive_lsb 10
-  def byte_offset, do: @offset
-  def protocol_name_length_msb, do: @protocol_name_length_msb
-  def protocol_name_length_lsb, do: @protocol_name_length_lsb
-  def protocol_name, do: @protocol_name
-  def protocol_level, do: @protocol_level
-  def msg_keepalive_msb, do: @msg_keepalive_msb
-  def msg_keepalive_lsb, do: @msg_keepalive_lsb
+  # def create_packet(type)
 
-  def create_packet(type)
-
-  def create_packet(:connect) do
-    # user name, password, will retain, will qos msb, will qos lsb, will flag, clean session, reserved
-    flags = [1, 1, 0, 0, 1, 1, 1, 0]
-    flags_byte = set_flags(flags, byte_offset(), 0)
+  def create_packet(:connect, :clean) do
+    # username, password, will retain, will qos msb, will qos lsb, will flag, clean session, reserved
+    flags = [0, 0, 0, 0, 0, 0, 1, 0]
+    flags_byte = set_flags(flags)
 
     variable_header =
       :binary.list_to_bin([
@@ -34,8 +20,7 @@ defmodule PacketCreator do
         msg_keepalive_lsb()
       ])
 
-    payload =
-      :binary.list_to_bin(preffix_length(["willtopic", "willmessage", "johndoe", "secret"], []))
+    payload = preffix_length(["boi1"], "")
 
     rem_length = RemLength.encode_rem_len([], byte_size(variable_header) + byte_size(payload))
 
@@ -51,10 +36,10 @@ defmodule PacketCreator do
   end
 
   def preffix_length([head | tail], payload) do
-    preffix_length(tail, payload ++ [byte_size(head), head])
+    preffix_length(tail, payload <> <<byte_size(head)::16>> <> head)
   end
 
-  def set_flags(flags_array, byte_offset, byte)
+  def set_flags(flags_array, byte_offset \\ 7, byte \\ 0)
 
   def set_flags([], _, byte) do
     byte
