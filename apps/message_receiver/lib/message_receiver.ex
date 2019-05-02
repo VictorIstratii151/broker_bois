dbb44f2da8f402d4819f5f28f61c0e8f8e9aae6f
Victor Istratii <victor.istratii@faf.utm.md>
2019-05-02 22:36:31 +0300

    The client can send SUBSCRIBE messages and receive SUBACK messages.

--- apps/message_receiver/lib/message_receiver.ex
+++ apps/message_receiver/lib/message_receiver.ex
@@ -4,7 +4,12 @@ defmodule MessageReceiver do
   # use GenServer
   require Logger
 
-  @local 'localhost'
+  def tst() do
+    socket = start(1337)
+    packet = PacketCreator.create_packet(:connect, :clean, "boi1")
+    IO.inspect(packet)
+    send_msg(socket, packet)
+  end
 
   def start(port) do
     opts = [:binary, packet: 0, active: false, reuseaddr: true]
@@ -30,16 +35,48 @@ defmodule MessageReceiver do
   def loop(socket) do
     case :gen_tcp.recv(socket, 0) do
       {:ok, packet} ->
-        IO.inspect("Inpsecting the packet:")
         IO.inspect(packet)
+        <<packet_type::binary-size(1), rest::binary>> = packet
+
+        case packet_type do
+          <<0x20>> ->
+            IO.inspect("Incoming CONNACK packet")
+
+            rem_length = RemLength.decode_rem_length(:binary.bin_to_list(rest))
+            fixed_header_length = byte_size(packet) - rem_length
+
+            <<_fixed::binary-size(fixed_header_length), _variable::binary>> = packet
+
+            response = PacketCreator.create_packet(:subscribe, "sooos1", 0)
+            send_msg(socket, response)
+
+          <<0x90>> ->
+            IO.inspect("Incoming SUBACK packet")
+            rem_length = RemLength.decode_rem_length(:binary.bin_to_list(rest))
+            fixed_header_length = byte_size(packet) - rem_length
+
+            <<_fixed::binary-size(fixed_header_length), variable_and_payload::binary>> = packet
+
+            <<packet_id::binary-size(2), return_code::binary>> = variable_and_payload
+
+            IO.inspect(packet_id)
+            IO.inspect(return_code)
+
+          _ ->
+            IO.inspect("sas")
+        end
+
+        loop(socket)
+
+      {:error, :enotconn} ->
+        :gen_tcp.close(socket)
 
       {:error, :closed} ->
-        Logger.info("Connection closed.")
+        IO.inspect("Connection closed.")
 
       something ->
         IO.inspect(something)
+        loop(socket)
     end
-
-    loop(socket)
   end
 end
