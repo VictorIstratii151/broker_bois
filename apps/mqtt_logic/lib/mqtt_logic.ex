3968fcb26d5400fcb24d03bcf30aee5861e012cf
Victor Istratii <victor.istratii@faf.utm.md>
2019-05-02 22:37:45 +0300

    Removed the offset global variable.

--- apps/mqtt_logic/lib/mqtt_logic.ex
+++ apps/mqtt_logic/lib/mqtt_logic.ex
@@ -1,18 +1,16 @@
 defmodule MqttLogic do
-  @moduledoc """
-  Documentation for MqttLogic.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MqttLogic.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @offset 7
+  @protocol_name_length_msb 0
+  @protocol_name_length_lsb 4
+  @protocol_name "MQTT"
+  @protocol_level 4
+  @msg_keepalive_msb 0
+  @msg_keepalive_lsb 10
+  def byte_offset, do: @offset
+  def protocol_name_length_msb, do: @protocol_name_length_msb
+  def protocol_name_length_lsb, do: @protocol_name_length_lsb
+  def protocol_name, do: @protocol_name
+  def protocol_level, do: @protocol_level
+  def msg_keepalive_msb, do: @msg_keepalive_msb
+  def msg_keepalive_lsb, do: @msg_keepalive_lsb
 end
